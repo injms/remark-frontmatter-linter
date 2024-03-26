f6009c6538a17c1cfb91eb2cfb3dcf8a606da5cc
Ian James <injms@users.noreply.github.com>
2024-03-26 15:48:27 +1100

    😌 Use satisfies instead of partial type check

--- src/defaults.ts
+++ src/defaults.ts
@@ -4,6 +4,6 @@ export const defaultExtends: string | string[] = [
   'plugin:yml/standard',
 ]
 
-export const defaultRules: Partial<Linter.RulesRecord> = {
+export const defaultRules = {
   indent: ['error', 2],
-}
+} satisfies Linter.RulesRecord
